01821edacc2ea8b376dd16b1019b9104d1449885
Hari Priyanka <jnet@hrmstest.jnetind.com>
2018-07-20 18:41:26 +0530

    First commit

---
+++ WetherReport/AddNewLocationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddNewLocationViewController.swift
+//  WetherReport
+//
+//  Created by Hari Priyanka on 20/07/18.
+//  Copyright © 2018 JNET. All rights reserved.
+//
+
+import UIKit
+
+class AddNewLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ WetherReport/cordataModel.swift
@@ -0,0 +1,13 @@
+//
+//  cordataModel.swift
+//  WetherReport
+//
+//  Created by Hari Priyanka on 20/07/18.
+//  Copyright © 2018 JNET. All rights reserved.
+//
+
+import UIKit
+
+class cordataModel: NSObject {
+
+}
