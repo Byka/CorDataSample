a421aadaa42367d47d0c5e3af11821b41bef1b3a
Hari Priyanka <jnet@hrmstest.jnetind.com>
2018-07-20 10:18:06 +0530

    Initial Commit

---
+++ WetherReport/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WetherReport
+//
+//  Created by Hari Priyanka on 20/07/18.
+//  Copyright © 2018 JNET. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
